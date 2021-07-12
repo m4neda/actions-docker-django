8d62d8ba7c0e7343ff68cbf0f4138f670f23ee47
m4neda <m4neda@gmail.com>
2021-07-12 17:03:44 +0900

    use default mysql

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'NAME': 'great_db',
         'USER': 'root',
         'PASSWORD': 'mysql',
-        'HOST': 'db',
-        'PORT': '3800',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
         }
