33e981fa77a41f8b00f7f8aad87c00d4c49bfad0
m4neda <m4neda@gmail.com>
2021-07-13 10:46:02 +0900

    try to create tables

--- client.py
+++ client.py
@@ -16,3 +16,27 @@ except mysql.connector.Error as err:
         print(err)
 else:
     cnx.close()
+
+conn = cnx
+print(conn.is_connected())
+cur = conn.cursor()
+
+# id, name, priceを持つテーブルを（すでにあればいったん消してから）作成
+table = 'test_table'
+cur.execute("DROP TABLE IF EXISTS `%s`;", table)
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS `%s` (
+    `id` int auto_increment primary key,
+    `name` varchar(50) not null,
+    `price` int(11) not null
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+    """, table)
+
+cur.execute("INSERT INTO test_table VALUES (1, 'BTC', 10200)")
+
+cur.execute("SELECT * FROM test_table ORDER BY id ASC")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
